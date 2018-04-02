daa5aaddb12f82fd734509b2f7f77e94b83c87fd
Rachel Elam <relam24@gmail.com>
2018-04-02 18:19:16 -0400

    synatax

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,67 @@
-const app = angular.module("GuitarApp", []);
+const app = angular.module('GuitarApp', []);
 
-app.controller("GuitarController", ["$http", function($http){
+app.controller('GuitarController', ['$http', function ($http) {
 
-    this.createForm = {};
-    this.guitar = "";
+	this.createForm = {};
+	this.guitar = '';
 
-    //create song entry
-    this.createGuitar = () => {
-        $http({
-            method: 'POST',
-            url: '/guitar',
-            data: this.createForm
-        }).then(response => {
-            this.guitars.push(response.data);
-            this.createForm ={};
-        }, error => {
-            console.error(error);
-        }).catch(err => console.error('Catch: ', err));
-    }
+// create song entry
+	this.createGuitar = () => {
+		$http({
+			method: 'POST',
+			url: '/guitar',
+			data: this.createForm
+		}).then(response => {
+			this.guitars.push(response.data);
+			this.createForm = {};
+		}, error => {
+			console.error(error);
+		}).catch(err => console.error('Catch: ', err));
+	};
 
-    //get index of songs
-    this.getGuitar = () => {
-        $http({
-            method: 'GET',
-            url: '/guitar'
-        }).then(response => {
-            this.guitars = response.data;
-        }, error => {
-            console.error(error);
-        }).catch(err => console.error('Catch: ', err))
-    };
+// get index of songs
+	this.getGuitar = () => {
+		$http({
+			method: 'GET',
+			url: '/guitar'
+		}).then(response => {
+			this.guitars = response.data;
+		}, error => {
+			console.error(error);
+		}).catch(err => console.error('Catch: ', err))
+	};
+	this.getGuitar();
 
-    this.getGuitar();
-
-    //delete song entry
-    this.deleteGuitar = (id) => {
-        $http({
-            method:'DELETE',
-            url:'/guitar/' + id
-        }).then(response => {
-            const removeByIndex = this.guitars.findIndex(guitar =>
+// delete song entry
+	this.deleteGuitar = (id) => {
+		$http({
+			method: 'DELETE',
+			url: '/guitar/' + id
+		}).then(response => {
+			const removeByIndex = this.guitars.findIndex(guitar =>
             guitar._id === id);
-            this.guitars.splice(removeByIndex, 1);
-        }).catch(err => console.error('Catch: ', err))
-    };
+			this.guitars.splice(removeByIndex, 1);
+		}).catch(err => console.error('Catch: ', err));
+	};
 
-    //edit song entry
-    this.editGuitar = (guitar) => {
-        guitar.learnedSong = !guitar.learnedSong;
-        $http({
-            method:'PUT',
-            url:'/guitars/' + guitar_.id,
-            data: {
-                name: this.updatedName,
-                artist: this.updatedArtist,
-                url: this.updatedUrl,
-                tabUrl: this.updatedtabUrl,
-                lyricUrl: this.updatedlyricUrl,
-                learnedSong: {learnedSong: this.updatedlearnedSong}
-            }
-        }).then( response => {
-            this.getGuitar();
-        }, error => {
-            console.error(error);
-        }).catch(err => console.log('Catch', error))
-    }
-
+// edit song entry
+	this.editGuitar = (guitar) => {
+		guitar.learnedSong = !guitar.learnedSong;
+		$http({
+			method: 'PUT',
+			url: '/guitars/' + guitar_.id,
+			data: {
+				name: this.updatedName,
+				artist: this.updatedArtist,
+				url: this.updatedUrl,
+				tabUrl: this.updatedtabUrl,
+				lyricUrl: this.updatedlyricUrl,
+				learnedSong: {learnedSong: this.updatedlearnedSong}
+			}
+		}).then(response => {
+			this.getGuitar();
+		}, error => {
+			console.error(error);
+		}).catch(err => console.log('Catch', error))
+	};
 }]);
