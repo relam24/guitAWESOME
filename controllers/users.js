d28400623f2fc8038c13d302d6357a64217df52d
Olena Penn <olenapenn@gmail.com>
2018-04-04 09:02:52 -1000

    sessions controller

--- controllers/users.js
+++ controllers/users.js
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
 	req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 	User.create(req.body, (err, createdUser) => {
-		// req.session.currentuser = createdUser;
-		// req.session.curentpassword = createdUser;
+		req.session.currentuser = createdUser;
+		req.session.curentpassword = createdUser;
 		res.status(201).json({
 			status: 201,
 			message: 'User Created'
