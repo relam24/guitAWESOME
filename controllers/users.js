661d64e2087364b4304c3711ef44c796861fbc17
Olena Penn <olenapenn@gmail.com>
2018-04-05 10:48:54 -1000

    fixed merge conflics, added some button and board styling

--- controllers/users.js
+++ controllers/users.js
@@ -25,11 +25,10 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
 	req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 	User.create(req.body, (err, createdUser) => {
-		// req.session.currentuser = createdUser;
-		// req.session.curentpassword = createdUser;
 		if (err) {
 			res.send('Please choose another username');
 		} else {
+			req.session.currentuser = createdUser;
 			res.status(201).json({
 				status: 201,
 				message: 'User Created'
--- models/guitar.js
+++ models/guitar.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const guitarScraper = require('ultimate-guitar-scraper');
 
 const guitarSchema = new mongoose.Schema({
-	name: {type: String, require: true},
-	artist: {type: String, require: true},
-	url: String,
-	tabUrl: {text: String, html: String},
-	lyricUrl: String,
-	learnedSong: {type: Boolean, default: false}
+	name: {type: String, required: true},
+	artist: {type: String, required: true},
+	url: {type: String, required: true},
+	tabUrl: {type: String, required: true},
+	lyricUrl: {type: String, required: true},
+	learnedSong: {type: Boolean, default: false, required: true}
 });
 
 const Guitar = mongoose.model('Guitar', guitarSchema);
